fba7917aeecceb39f51757ddc377eef13edc4dcb
altynbek_y <altynbek_y@auca.kg>
2022-09-09 16:57:11 +0200

    lab01 p01 part 1 completed

--- lab01/p01/main.cpp
+++ lab01/p01/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
 
 int main()
 {
+    std::ofstream out("hello17.txt");
+    std::streambuf *coutbuf = std::cout.rdbuf();    // save old buffer
+    std::cout.rdbuf(out.rdbuf());                   // redirect std::cout to hello11.txt
+
     std::cout << "Hello, C++ " << __cplusplus << "!\n";
+
+    std::cout.rdbuf(coutbuf);                       // reset to standard to output again
 }

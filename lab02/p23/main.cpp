1a7ca49034e3f642ce27dda833c6c771794aa5ba
altynbek_y <altynbek_y@auca.kg>
2022-09-30 12:52:34 +0200

    lab-02 p23 started

---
+++ lab02/p23/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int i, j, k;
+    string b, t;
+
+    while (cin >> b >> t)
+    {
+        for(i = 0; i < sz(b)-sz(t); i++)
+        {
+            for (i = 0; i < count; i++)
+            {
+                /* code */
+            }
+            
+        }
+
+        cout << "\n";
+    }
+}

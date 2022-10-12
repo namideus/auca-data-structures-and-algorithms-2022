a35859f13cdad410c1c4d145c40503a0b600ab83
altynbek_y <altynbek_y@auca.kg>
2022-10-12 09:58:11 +0200

    lab-02 p35 accepted answer (improved the code)

--- lab02/p35/main.cpp
+++ lab02/p35/main.cpp
@@ -12,14 +12,12 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-    int i, k, j, n, f, ans, b, t, p1, p2, p3;
+    int i, j, n, f, ans, b, t, p1, p2, p3;
     char c, l;
     string txt;
-    string k1 = "qwertyuiop[]";
-    string k2 = "asdfghjkl;'";
-    string k3 = "zxcvbnm,.";
+    string k = "qwertyuiop[]asdfghjkl;'zxcvbnm,.";
 
-    while (getline(cin, txt, '\n'))
+    while (getline(cin, txt))
     {
         for (i = 0; i < sz(txt); ++i)
         {
@@ -29,19 +27,10 @@ int main()
                 cout << c;
             else
             {
-                p1 = k1.find(tolower(c));
-                p2 = k2.find(tolower(c));
-                p3 = k3.find(tolower(c));
-
-                if (p1 != string::npos)
-                    cout << k1[p1 - 2];
-                else if (p2 != string::npos)
-                    cout << k2[p2 - 2];
-                else if (p3 != string::npos)
-                    cout << k3[p3 - 2];
+                p1 = k.find(tolower(c));
+                cout << k[p1 - 2];
             }
         }
     }
-
     cout << "\n";
 }

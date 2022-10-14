4d5affd3524e6115eff4869c1a499b524e249a27
altynbek_y <altynbek_y@auca.kg>
2022-10-14 18:47:30 +0200

    lab-02 p29 wrong answer

--- lab02/p29/main.cpp
+++ lab02/p29/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define INF 1e9
 
 template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
@@ -11,34 +12,43 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-    int i, j, n, f, ans, maxCnt;
+    int i, j, k, n, f, ans, c, min_cnt;
     string str;
-    char c, l;
 
     vector<string> v;
-    map<string, int> cnt;
 
     while (cin >> str && str != "#")
     {
-        maxCnt = 0;
-
         if (str[0] == 'e')
         {
+            min_cnt = INF;
+
             for (i = 0; i < sz(v); ++i)
             {
-                cnt[v[i]]++;
+                c = 0;
 
-                if (cnt[v[i]] >= maxCnt)
+                for (j = 0; j < sz(v); ++j)
                 {
+                    if (i != j)
+                    {
+                        for (k = 1; k <= 17; k += 4)
+                            if (v[i][k - 1] != v[j][k - 1] || v[i][k + 1] != v[j][k + 1])
+                                c++;
+                    }
+                }
+
+                // cout << "cnt = " << c <<"\n";
+
+                if (c <= min_cnt)
+                {
+                    min_cnt = c;
                     ans = i + 1;
-                    maxCnt = cnt[v[i]];
                 }
             }
 
             cout << ans << "\n";
 
             v.clear();
-            cnt.clear();
         }
         else
             v.push_back(str);

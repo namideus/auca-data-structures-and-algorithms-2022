6c4267accd40c8e16b6971a9be2fbd6d247c48c1
altynbek_y <altynbek_y@auca.kg>
2022-10-17 06:48:44 +0200

    lab-02 p23 accepted answer

--- lab02/p23/main.cpp
+++ lab02/p23/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define INF 1e9
 
 template <typename C>
 int sz(const C &c) { return static_cast<int>(c.size()); }
@@ -7,22 +8,86 @@ using namespace std;
 
 int main()
 {
-    iostream::sync_with_stdio(false);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    int i, j, k;
-    string b, t;
+    string s;
+    int i, k, j, n, l, f, ans, b, t;
 
-    while (cin >> b >> t)
+    for (string btm, top; cin >> btm >> top;)
     {
-        for(i = 0; i < sz(b)-sz(t); i++)
+        if (sz(btm) < sz(top))
+            swap(btm, top);
+
+        ans = -1;
+        n = sz(top);
+        l = sz(btm);
+
+        for (i = 0; i < l - n + 1; ++i)
         {
-            for (i = 0; i < count; i++)
+            s = btm.substr(i, n);
+            f = 1;
+
+            for (j = 0; j < n; ++j)
+                if (s[j] == top[j] && s[j] == '2')
+                {
+                    f = 0;
+                    break;
+                }
+
+            if (f)
             {
-                /* code */
+                ans = l;
+                break;
             }
-            
         }
 
-        cout << "\n";
+        if (ans == -1)
+        {
+            ans = INF;
+
+            for (k = 1; k < n; ++k)
+            {
+                s = top.substr(n - k, n);
+                f = 1;
+
+                for (i = 0; i < k; ++i)
+                    if (s[i] == btm[i] && s[i] == '2')
+                    {
+                        f = 0;
+                        break;
+                    }
+
+                if (f)
+                {
+                    ans = min(n - k + l, ans);
+                }
+            }
+
+            for (k = 1; k < n; ++k)
+            {
+                s = top.substr(0, k);
+
+                f = 1;
+
+                for (i = l - k, j = 0; i < l; ++i, ++j)
+                    if (s[j] == btm[i] && s[j] == '2')
+                    {
+                        f = 0;
+                        break;
+                    }
+
+                if (f)
+                {
+                    ans = min(n - k + l, ans);
+                }
+            }
+        }
+
+        if (ans != INF)
+            cout << ans << "\n";
+        else
+            cout << l + n << "\n";
     }
 }

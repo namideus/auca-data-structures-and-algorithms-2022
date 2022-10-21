730d90b253ecd9305dd7d568d9e708311b5e7f4e
altynbek_y <altynbek_y@auca.kg>
2022-10-21 12:58:37 +0200

    lab-03 p01 accepted answer

--- lab03/p01/main.cpp
+++ lab03/p01/main.cpp
@@ -36,3 +36,51 @@ int main()
         printDeck(deck);
     }
 }
+
+vector<vector<int>> readShuffles()
+{
+    int n; cin >> n;
+    vector<vector<int>> shuffles(n, vector<int>(52));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 52; j++)
+        {
+            cin >> shuffles[i][j];
+        }
+        cin.ignore(1024, '\n');
+    }
+
+    return shuffles;   
+}
+
+void applyShuffleDeck(vector<int> &deck, const vector<int> &shuffle)
+{
+    vector<int> new_deck(52);
+
+    for (int i = 0; i < 52; i++)
+    {
+        new_deck[i] = deck[shuffle[i]-1];
+    }
+    
+    deck = new_deck;
+}
+
+vector<int> createDeck()
+{
+    vector<int> deck(52);
+
+    for (int i = 0; i < sz(deck); i++)
+        deck[i] = i;
+
+    return deck;
+}
+
+void printDeck(const vector<int> &deck)
+{
+    static const vector<string> suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    static const vector<string> ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+    for (auto card : deck)
+        cout << ranks[card%13] << " of " << suits[card/13] << "\n";
+}

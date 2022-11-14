89558fd8980ce145493fa923bb548f3607484835
altynbek_y <altynbek_y@auca.kg>
2022-11-14 03:35:23 +0100

    project01: added check for negative additives in operator +

--- project01/BigInt/BigInt.hpp
+++ project01/BigInt/BigInt.hpp
@@ -5,6 +5,7 @@ class BigInt
     friend std::ostream &operator<<(std::ostream &, const BigInt &);
     friend std::istream &operator>>(std::istream &, const BigInt &);
     friend BigInt operator+(const BigInt &, const BigInt &);
+    friend BigInt operator-(const BigInt &, const BigInt &);
 
     std::vector<int> mDigits;
     bool mIsNegative;
@@ -85,6 +86,50 @@ inline BigInt operator+(const BigInt &b1, const BigInt &b2)
 
     for (auto d : b)
         sum += std::to_string(d);
+
+    if (b1.mIsNegative && b2.mIsNegative)
+        sum.insert(sum.begin(), '-');
+    else if (b1.mIsNegative || b2.mIsNegative)
+
+        return BigInt(sum);
+}
+
+inline BigInt operator-(const BigInt &b1, const BigInt &b2)
+{
+    std::string sum = "";
+    std::vector<int> a = b1.mDigits;
+    std::vector<int> b = b2.mDigits;
+
+    int size_a = (int)a.size();
+    int size_b = (int)b.size();
+
+    std::vector<int> z(std::abs(size_a - size_b), 0);
+
+    if (size_a > size_b)
+        b.insert(b.begin(), z.begin(), z.end());
+    else if (size_b > size_a)
+        a.insert(a.begin(), z.begin(), z.end());
+
+    for (int i = std::max(size_a, size_b) - 1; i >= 1; i--)
+    {
+        b[i] += a[i];
+        b[i - 1] += b[i] / 10;
+        b[i] %= 10;
+    }
+
+    b[0] += a[0];
+    if (b[0] / 10 != 0)
+    {
+        int tmp = b[0];
+        b[0] = tmp % 10;
+        b.insert(b.begin(), tmp / 10);
+    }
+
+    for (auto d : b)
+        sum += std::to_string(d);
+
+    if (b1.mIsNegative && b2.mIsNegative)
+        sum.insert(sum.begin(), '-');
 
     return BigInt(sum);
 }

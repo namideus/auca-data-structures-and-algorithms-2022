9f4cfbdf516a61ce926fe8d78e829380b8c03b2a
altynbek_y <altynbek_y@auca.kg>
2022-12-02 08:48:20 +0100

    project02 p02 accepted answer

---
+++ project02/p01/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+struct CompByMod
+{
+    int m;
+
+    CompByMod(int aM) : m(aM) {}
+
+    bool operator()(const int a, const int b) const
+    {
+        int rA = a % m;
+        int rB = b % m;
+
+        if(rA != rB)
+        {
+            return rA < rB;
+        }
+
+        if(a % 2 != 0 && b % 2 != 0)
+        {
+            return a > b;
+        }
+
+        if(a % 2 != 0 && b % 2 == 0)
+        {
+            return true;
+        }
+
+        if(a % 2 == 0 && b % 2 != 0)
+        {
+            return false;
+        }
+
+        return a < b;
+    }
+};
+
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int n, m;
+
+    while(cin >> n >> m && n + m > 0)
+    {
+        vector<int> v(n);
+
+        for(auto & x : v)
+            cin >> x;
+
+        sort(begin(v), end(v), [m](int a, int b)]);
+
+        cout << n << " " << m << "\n";
+
+        for(auto d : v)
+            cout << d << "\n";
+    }
+
+    cout << "0 0\n";
+}

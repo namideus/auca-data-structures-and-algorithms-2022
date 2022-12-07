bb0a272d0acd3f489c158c17f0c0e3d0a52fa021
altynbek_y <altynbek_y@auca.kg>
2022-12-07 08:01:17 +0100

    project02: p14 accepted answer

---
+++ project02/p14/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    string s;
+    bool f = true;
+
+    while (cin >> n && n)
+    {
+        if (!f)
+            cout << "\n";
+
+        f = false;
+
+        vector<string> v(n);
+
+        for (auto &x : v)
+            cin >> x;
+
+        stable_sort(begin(v), end(v), [](const string &a, const string &b)
+                    { return a.substr(0, 2) < b.substr(0, 2); });
+
+        for (auto x : v)
+            cout << x << "\n";
+    }
+}

cd08acff26f554180904852b7048d73414942286
altynbek_y <altynbek_y@auca.kg>
2022-12-14 05:22:28 +0100

    lab05: started implementing

---
+++ lab05/p01/main.cpp
@@ -0,0 +1,68 @@
+#include "../../au/algol.hpp"
+
+#include "VecInt.hpp"
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+void p01();
+void p02();
+void printVecInt(const VecInt &v);
+
+int main()
+{
+    try
+    {
+        // p01();
+        p02();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+}
+
+void p01()
+{
+    cout << "size of array: ";
+
+    int n;
+    cin >> n;
+
+    VecInt v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    auReverse(begin(v), end(v));
+    // auReverse(v.begin(), v.end());
+
+    printVecInt(v);
+}
+
+void p02()
+{
+    std::cout << "enter integer numbers: " << std::endl;
+
+    VecInt v;
+    for (int x; cin >> x;)
+    {
+        v.pushBack(x);
+    }
+
+    printVecInt(v);
+    auReverse(v.begin(), v.end());
+    printVecInt(v);
+}
+
+void printVecInt(const VecInt &v)
+{
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
